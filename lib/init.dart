cd04681600feb172198569d361147643b2d534ed
MohdFarag <mohamed.mahmoud02@eng-st.cu.edu.eg>
2024-06-21 06:29:32 +0300

    chore: Remove unused environment variables and files related to Firebase options

--- lib/init.dart
+++ lib/init.dart
@@ -1,11 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-import 'firebase_options.dart';
+import 'src/core/configs/firebase_options.dart';
 
 Future<void> init() async {
   // Load the environment variables
-  await dotenv.load();
+  await dotenv.load(); // Todo: Check path to .env file
 
   // Initialize Firebase
   await Firebase.initializeApp(
